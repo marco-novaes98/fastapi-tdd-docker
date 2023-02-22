9fec560b76c1f45b9ff261d587977dc903a1c804
Marco Novaes <marco.novaes@piperlab.es>
2023-02-22 17:37:14 +0100

    Part 1 - FastAPI and Docker

---
+++ project/tests/conftest.py
@@ -0,0 +1,21 @@
+import os
+
+import pytest
+from starlette.testclient import TestClient
+
+from app import main
+from app.main import create_application  # updated
+from app.config import get_settings, Settings
+
+
+def get_settings_override():
+    return Settings(testing=1, database_url=os.environ.get("DATABASE_TEST_URL"))
+
+
+@pytest.fixture(scope="module")
+def test_app():
+    app = create_application()  # new
+    app.dependency_overrides[get_settings] = get_settings_override
+    with TestClient(app) as test_client:  # updated
+        # testing
+        yield test_client
